df06935ca852970ebe8779bdc15e2696701ffd30
Vitaliy <GarazdVZ@gmail.com>
2015-05-28 22:12:07 +0300

    Add DoubleCheckedLocking

---
+++ src/DoubleCheckedLocking.java
@@ -0,0 +1,16 @@
+public class DoubleCheckedLocking {
+    private static DoubleCheckedLocking instance;
+    private DoubleCheckedLocking() {
+    }
+    private synchronized static void doSync() {
+        //It is only to synchronize
+    }
+    public static DoubleCheckedLocking getInstance() {
+        if (instance == null) {
+            DoubleCheckedLocking.doSync();
+            if (instance == null)
+                instance = new DoubleCheckedLocking();
+        }
+        return instance;
+    }
+}
--- src/Singleton.java
+++ src/Singleton.java
@@ -1,6 +1,7 @@
 public class Singleton {
         private static Singleton instance;
-        private Singleton(){}
+        private Singleton(){
+        }
         public static Singleton getInstance() {
         if (instance == null)
             instance = new Singleton();
