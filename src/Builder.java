d5cd74c9aa4f618cc708ef8dfe7ba013920f3fc8
Vitaliy <GarazdVZ@gmail.com>
2015-06-01 09:40:24 +0300

    Update Builder

--- src/Builder.java
+++ src/Builder.java
@@ -5,15 +5,64 @@ public class Builder {
     private static String phone;
     private static String address;
 
-    public Builder(final String name,
-                   final String surname,
-                   final String mail,
-                   final String phone,
-                   final String address) {
-        this.name = name;
+    Builder(final ConcreteBuilder concreteBuilder) {
+        this.name = concreteBuilder.getName();
         this.surname = surname;
         this.mail = mail;
         this.phone = phone;
         this.address = address;
+    }
+}
+
+public class ConcreteBuilder {
+    private static String name;
+    private static String surname;
+    private static String mail;
+    private static String phone;
+    private static String address;
+
+    public ConcreteBuilder name(final String name){
+        this.name = name;
+        return this;
+    }
+
+    public ConcreteBuilder surname(final String surname){
+        this.surname = surname;
+        return this;
+    }
+
+    public ConcreteBuilder mail(final String mail){
+        this.mail = mail;
+        return this;
+    }
+
+    public ConcreteBuilder phone(final String phone){
+        this.phone = phone;
+        return this;
+    }
+
+    public ConcreteBuilder address(final String address){
+        this.address = address;
+        return this;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static String getSurname() {
+        return surname;
+    }
+
+    public static String getMail() {
+        return mail;
+    }
+
+    public static String getPhone() {
+        return phone;
+    }
+
+    public static String getAddress() {
+        return address;
     }
 }
