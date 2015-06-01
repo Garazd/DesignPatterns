350be277c095e44c34d0ae55ae70afd3da93d4ce
Vitaliy <GarazdVZ@gmail.com>
2015-06-01 09:04:12 +0300

    Update Builder

--- src/Builder.java
+++ src/Builder.java
@@ -1,2 +1,19 @@
 public class Builder {
+    private static String name;
+    private static String surname;
+    private static String mail;
+    private static String phone;
+    private static String address;
+
+    public Builder(final String name,
+                   final String surname,
+                   final String mail,
+                   final String phone,
+                   final String address) {
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+        this.phone = phone;
+        this.address = address;
+    }
 }
