267e5fd5bce0c8b4db4154d4d8a73345e40a16fe
Vitaliy <GarazdVZ@gmail.com>
2015-06-03 22:50:00 +0300

    Update DoubleCheckedLocking

--- src/DoubleCheckedLocking.java
+++ src/DoubleCheckedLocking.java
@@ -1,10 +1,14 @@
 public class DoubleCheckedLocking {
+
     private static DoubleCheckedLocking instance;
+
     private DoubleCheckedLocking() {
     }
+
     private synchronized static void doSync() {
         //It is only to synchronize
     }
+
     public static DoubleCheckedLocking getInstance() {
         if (instance == null) {
             DoubleCheckedLocking.doSync();
