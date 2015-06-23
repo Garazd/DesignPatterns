19c77ee38bbaeb3d9ea941e6b928517754d8fafb
Vitaliy <GarazdVZ@gmail.com>
2015-06-23 21:25:38 +0300

    Update Singleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,12 +1,10 @@
 public class Singleton {
-    private static Singleton instance;
+    private static Singleton instance = new Singleton();
 
     private Singleton(){
     }
 
     public static Singleton getInstance() {
-        if (instance == null)
-            instance = new Singleton();
         return instance;
     }
 }
