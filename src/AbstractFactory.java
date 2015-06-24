d09b0fb1a6ed753cb771aa274d4400f0dbdb9c67
Vitaliy <GarazdVZ@gmail.com>
2015-06-24 21:25:55 +0300

    Update AbstractFactory

--- src/AbstractFactory.java
+++ src/AbstractFactory.java
@@ -1,2 +1,32 @@
-public class AbstractFactory {
+class WindowsFactory implements AbstractFactory {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    @Override
+    public Label createLabel() {
+        return new WindowsLabel();
+    }
+}
+
+class MacOSFactory implements AbstractFactory {
+    @Override
+    public Button createButton() {
+        return new ;
+    }
+}
+
+interface Button {
+    void button();
+}
+
+interface Label {
+    void label();
+}
+
+interface AbstractFactory {
+    Button createButton();
+    Label createLabel();
 }
