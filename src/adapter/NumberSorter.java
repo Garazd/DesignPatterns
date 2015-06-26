a54bd1162da64d435b19163dca493378cce6eb57
Vitaliy <GarazdVZ@gmail.com>
2015-06-26 20:47:20 +0300

    Add Adapter

---
+++ src/adapter/NumberSorter.java
@@ -0,0 +1,10 @@
+package adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberSorter {
+    public List<Integer> sort(List<Integer> numbers) {
+        return new ArrayList<Integer>();
+    }
+}
