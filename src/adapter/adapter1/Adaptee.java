e1ddd9017b0e6e4b43879e645b8e8bc1874872a0
Vitaliy <GarazdVZ@gmail.com>
2015-08-24 21:32:27 +0300

    Add Adapter1

---
+++ src/adapter/adapter1/Adaptee.java
@@ -0,0 +1,10 @@
+package adapter.adapter1;
+
+public class Adaptee {
+
+    public Object specificRequest(Object input) {
+        System.out.println("Call Adaptee.specificRequest(" + input + ")");
+        return "Hello from Adaptee!";
+    }
+    
+}
---
+++ src/adapter/adapter1/Target.java
@@ -0,0 +1,5 @@
+package adapter.adapter1;
+
+public interface Target {
+    Object request(Object input);
+}
