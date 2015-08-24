ea90d2740631cf03c00cc75c746507e1c8428c10
Vitaliy <GarazdVZ@gmail.com>
2015-08-24 21:36:53 +0300

    Add Adapter5

---
+++ src/adapter/adapter5/Client.java
@@ -0,0 +1,19 @@
+package adapter.adapter5;
+
+public class Client {
+    public static void main(String[] args) {
+        final Adaptee adaptee = new Adaptee();
+
+        Target target = new Target() {
+            public Object request(Object input) {
+                return adaptee.specificRequest(input);
+            }
+        };
+
+        doit(target);
+    }
+
+    public static void doit(Target target) {
+        System.out.println(target.request("Hello World!"));
+    }
+}
---
+++ src/adapter/adapter5/Target.java
@@ -0,0 +1,6 @@
+package adapter.adapter5;
+
+public interface Target {
+
+    Object request(Object input);
+}
