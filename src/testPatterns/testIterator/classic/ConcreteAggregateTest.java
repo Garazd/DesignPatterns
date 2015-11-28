98c2be6875bc3718a161fb65740a6d5abaf792f3
Garazd <GarazdVZ@gmail.com>
2015-11-28 22:54:17 +0200

    Changed deprecated import

--- src/testPatterns/testIterator/classic/ConcreteAggregateTest.java
+++ src/testPatterns/testIterator/classic/ConcreteAggregateTest.java
@@ -6,7 +6,7 @@ import designPatterns.iterator.classic.Iterator;
 
 import java.util.NoSuchElementException;
 
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
